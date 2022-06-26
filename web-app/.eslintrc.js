78897bb158c9956e0967a1c123efe9fa782c77c4
Michael Chan <mhcchan@ucdavis.edu>
2022-06-25 19:30:22 -0700

    chore: fixes eslint issues

---
+++ web-app/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ['next', 'next/core-web-vitals'],
+  // rules: {
+  //   'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+  //   'react/jsx-props-no-spreading': [0],
+  // },
+};
